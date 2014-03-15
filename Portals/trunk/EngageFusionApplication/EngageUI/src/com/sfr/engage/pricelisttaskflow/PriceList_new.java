e20f45ac9baaf9df9decdb7eabc3f2cfde8aeb93
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-03-15 14:11:10 +0000

    hitk : css fixed for chrome except button and sessiop replication changes done on pricelist bean
    git-svn-id: https://10.24.239.14:5511/svn/Engage@151 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/pricelisttaskflow/PriceList_new.java
+++ Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/pricelisttaskflow/PriceList_new.java
@@ -1,11 +1,11 @@
 package com.sfr.engage.pricelisttaskflow;
 
+
 import com.sfr.engage.utility.util.Conversion;
 
+import java.io.Serializable;
+
 import java.util.Locale;
-
-
-import oracle.adf.view.rich.component.rich.data.RichTable;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -16,33 +16,43 @@ import javax.servlet.http.HttpSession;
 import oracle.adf.model.BindingContext;
 import oracle.adf.model.binding.DCBindingContainer;
 import oracle.adf.model.binding.DCIteratorBinding;
-
-
-import oracle.adf.view.rich.component.rich.output.RichOutputText;
-import oracle.adf.view.rich.component.rich.output.RichSpacer;
-import oracle.adf.view.rich.render.ClientEvent;
-
 import oracle.adf.view.rich.component.rich.data.RichTable;
 
 import oracle.jbo.ViewObject;
 
-public class PriceList_new {
-    private RichTable pricelisttable;
+
+public class PriceList_new implements Serializable{
+    @SuppressWarnings("compatibility")
+    private static final long serialVersionUID = 1L;
+    
+    private transient Bindings bindings;
     private Locale locale = new Locale("sv", "SE");
     private String country;
     private String currency_code;
-
+//    private static ADFLogger logger = ADFLogger.createADFLogger("En_portal");
+    
+    /**
+     * @return bindings Object
+     */
+    public Bindings getBindings() {
+        if (bindings == null) {
+            bindings = new Bindings();
+        }
+        return bindings;
+    }
+    
     public PriceList_new() {
         
         ExternalContext ectx = FacesContext.getCurrentInstance().getExternalContext();
         HttpServletRequest request = (HttpServletRequest)ectx.getRequest();
         HttpSession session = (HttpSession)request.getSession();
         //country = (String)session.getAttribute("lang");
-
+        
         country = "NO";
         Conversion convert = new Conversion();
         currency_code = convert.getCurrencyCode(country);
         System.out.println("Currency code " + currency_code);
+//        logger.info("Currency code " + currency_code);
         DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
         DCIteratorBinding iter;
         if (bindings != null) {
@@ -58,35 +68,72 @@ public class PriceList_new {
         vo.executeQuery();
     }
 
-    public void setPricelisttable(RichTable pricelisttable) {
-        this.pricelisttable = pricelisttable;
-    }
 
-    public RichTable getPricelisttable() {
-        return pricelisttable;
-    }
-
+    /**
+     * @param locale
+     */
     public void setLocale(Locale locale) {
         this.locale = locale;
     }
 
+    /**
+     * @return
+     */
     public Locale getLocale() {
         return locale;
     }
 
+    /**
+     * @param country
+     */
     public void setCountry(String country) {
         this.country = country;
     }
 
+    /**
+     * @return
+     */
     public String getCountry() {
         return country;
     }
 
+    /**
+     * @param currency_code
+     */
     public void setCurrency_code(String currency_code) {
         this.currency_code = currency_code;
     }
 
+    /**
+     * @return
+     */
     public String getCurrency_code() {
         return currency_code;
+    }
+
+//    public static void setLogger(ADFLogger logger) {
+//        PriceList_new.logger = logger;
+//    }
+//
+//    public static ADFLogger getLogger() {
+//        return logger;
+//    }
+    public class Bindings {
+        private RichTable pricelisttable;
+
+        /**
+         * @param pricelisttable
+         */
+        public void setPricelisttable(RichTable pricelisttable) {
+            this.pricelisttable = pricelisttable;
+        }
+
+        /**
+         * @return
+         */
+        public RichTable getPricelisttable() {
+            return pricelisttable;
+        }
+        
     }
 }
