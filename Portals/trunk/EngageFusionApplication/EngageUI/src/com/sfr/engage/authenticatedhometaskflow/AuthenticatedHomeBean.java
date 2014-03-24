7c10e0ca37e1a944ae4859709495a611d397101f
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-03-24 06:58:29 +0000

    PDEE: 03/24/2014 Information & messages from statoil is committed.
    git-svn-id: https://10.24.239.14:5511/svn/Engage@182 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/authenticatedhometaskflow/AuthenticatedHomeBean.java
+++ Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/authenticatedhometaskflow/AuthenticatedHomeBean.java
@@ -1,7 +1,75 @@
 package com.sfr.engage.authenticatedhometaskflow;
 
-public class AuthenticatedHomeBean {
+
+import com.sfr.engage.model.queries.rvo.PrtGenStringRVORowImpl;
+
+import com.sfr.engage.utility.util.ADFUtils;
+
+import com.sfr.engage.vehicleinfotaskflow.VehicleInfoBean;
+
+import java.io.Serializable;
+
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+
+import oracle.jbo.ViewObject;
+
+public class AuthenticatedHomeBean implements Serializable {
+    @SuppressWarnings("compatibility")
+    private static final long serialVersionUID = 1L;
+    private transient Bindings bindings;
+    private String Infovalue;
+
+    /**
+     * @return bindings Object
+     */
+    public Bindings getBindings() {
+        if (bindings == null) {
+            bindings = new Bindings();
+        }
+        return bindings;
+    }
+
     public AuthenticatedHomeBean() {
-        super();
+        ViewObject vo = ADFUtils.getViewObject("PrtGenStringRVO1Iterator");
+        System.out.println("query=====>" + vo.getQuery());
+        vo.setWhereClause("ID =:var");
+        vo.defineNamedWhereClauseParam("var", 102613, null);
+        vo.executeQuery();
+
+        if (vo.getEstimatedRowCount() != 0) {
+            System.out.println("coming inside this block");
+            for (int j = 0; j < vo.getEstimatedRowCount(); j++) {
+                while (vo.hasNext()) {
+                    PrtGenStringRVORowImpl currRow =
+                        (PrtGenStringRVORowImpl)vo.next();
+
+                    if (currRow != null) {
+                        Infovalue = (String)currRow.getAttribute("KeyValue");
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void setInfovalue(String Infovalue) {
+        this.Infovalue = Infovalue;
+    }
+
+    public String getInfovalue() {
+        return Infovalue;
+    }
+
+    public class Bindings {
+        private RichOutputText infoText;
+
+        public void setInfoText(RichOutputText infoText) {
+            this.infoText = infoText;
+        }
+
+        public RichOutputText getInfoText() {
+            return infoText;
+        }
     }
 }
+
