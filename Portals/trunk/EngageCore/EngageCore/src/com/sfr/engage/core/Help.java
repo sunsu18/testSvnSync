1a7e894ca98e49855cb6ff45472f5f1dfa5ff0a1
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-03-24 07:55:54 +0000

    hitk : FAQ related commits
    git-svn-id: https://10.24.239.14:5511/svn/Engage@185 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/Help.java
@@ -0,0 +1,42 @@
+package com.sfr.engage.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Help {
+    
+    public Help() {
+        super();
+    }
+     private String  question;
+     private String answer;
+
+
+    /**
+     * @param question
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * @return
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * @param answer
+     */
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    /**
+     * @return
+     */
+    public String getAnswer() {
+        return answer;
+    }
+}
