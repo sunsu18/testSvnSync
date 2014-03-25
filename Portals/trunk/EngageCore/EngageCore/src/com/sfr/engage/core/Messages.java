8333c9673aa506d613882173496269460cfee3d5
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-03-25 05:18:22 +0000

    PDEE: 03/25/2014 Reusable Messages Class added.
    git-svn-id: https://10.24.239.14:5511/svn/Engage@198 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/Messages.java
@@ -0,0 +1,22 @@
+package com.sfr.engage.core;
+
+public class Messages {
+    private String message;
+    public Messages() {
+        super();
+    }
+
+    /**
+     * @param message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return
+     */
+    public String getMessage() {
+        return message;
+    }
+}
