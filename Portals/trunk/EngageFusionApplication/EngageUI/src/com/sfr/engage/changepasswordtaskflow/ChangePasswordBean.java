01c9c29ecb801d4dfcfcf2ab5bf888d033c2b109
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-03-27 10:20:39 +0000

    PDEE: 03/27/2014 UI creation for change password.
    git-svn-id: https://10.24.239.14:5511/svn/Engage@232 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/changepasswordtaskflow/ChangePasswordBean.java
+++ Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/changepasswordtaskflow/ChangePasswordBean.java
@@ -1,7 +1,65 @@
 package com.sfr.engage.changepasswordtaskflow;
 
-public class ChangePasswordBean {
+import com.sfr.engage.authenticatedhometaskflow.AuthenticatedHomeBean;
+
+import java.io.Serializable;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+
+public class ChangePasswordBean implements Serializable  {
+    
+    @SuppressWarnings("compatibility")
+    private static final long serialVersionUID = 1L;
+    private transient Bindings bindings;
+    
+    /**
+     * @return bindings Object
+     */
+    public Bindings getBindings() {
+        if (bindings == null) {
+            bindings = new Bindings();
+        }
+        return bindings;
+    }
     public ChangePasswordBean() {
         super();
     }
+    
+    public class Bindings {
+        
+        private RichInputText oldPasswordIT;
+        private RichInputText newPasswordIT;
+        private RichInputText confirmPasswordIT;
+
+        public void setOldPasswordIT(RichInputText oldPasswordIT) {
+            this.oldPasswordIT = oldPasswordIT;
+        }
+
+        public RichInputText getOldPasswordIT() {
+            return oldPasswordIT;
+        }
+
+        public void setNewPasswordIT(RichInputText newPasswordIT) {
+            this.newPasswordIT = newPasswordIT;
+        }
+
+        public RichInputText getNewPasswordIT() {
+            return newPasswordIT;
+        }
+
+        public void setConfirmPasswordIT(RichInputText confirmPasswordIT) {
+            this.confirmPasswordIT = confirmPasswordIT;
+        }
+
+        public RichInputText getConfirmPasswordIT() {
+            return confirmPasswordIT;
+        }
+    }
+    
+
+  
 }
+
