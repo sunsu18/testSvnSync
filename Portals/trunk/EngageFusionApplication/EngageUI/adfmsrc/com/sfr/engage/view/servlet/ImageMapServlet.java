e23e30c0b1b5b95b8b89e411f0eb6464b8a936ea
ashtha <ashtha@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-03-28 14:42:04 +0000

    git-svn-id: https://10.24.239.14:5511/svn/Engage@268 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageUI/adfmsrc/com/sfr/engage/view/servlet/ImageMapServlet.java
+++ Portals/trunk/EngageFusionApplication/EngageUI/adfmsrc/com/sfr/engage/view/servlet/ImageMapServlet.java
@@ -1,6 +1,5 @@
 package com.sfr.engage.view.servlet;
 
-import com.sfr.engage.services.core.dao.factory.DAOFactory;
 import com.sfr.engage.services.core.dao.factory.DAOFactory.*;
 
 import java.io.BufferedInputStream;
@@ -58,7 +57,7 @@ public class ImageMapServlet extends HttpServlet{
         String Control_Attr_Value;
         if(Feature_Name.equalsIgnoreCase("LISTPRICE"))
         {
-            Control_Attr_Value = "41S5" + Attr_Value;
+            Control_Attr_Value = "41S2" + Attr_Value;
             System.out.println("Control_Attr_Value" + Control_Attr_Value);
         }
         else{
@@ -70,55 +69,7 @@ public class ImageMapServlet extends HttpServlet{
         try {
                 if (Feature_Name != null && Feature_Value!= null && Control_Attr_Value != null) 
                 {
-                    Connection1 = DAOFactory.getJNDIConnection();
-                    PreparedStatement statement = Connection1.prepareStatement("SELECT imageid from PRT_GEN_IMAGE_MAP where feature_name=? and feature_value=? and control_attr_value=?");
-                    statement.setString(1, Feature_Name);
-                    statement.setString(2, Feature_Value);
-                   // statement.setString(3, Control_Attr);
-                    statement.setString(3, Control_Attr_Value);
-                    //statement.setString(5, lang);
-
-                    //statement.setInt(1, new Integer(imageId));
-                    ResultSet rs = statement.executeQuery();
-                    if (rs.next()) {
-                        System.out.println("record found in image map table with image id " + rs.getString("imageid"));
-                        String image_id = rs.getString("imageid");
-                        
-                        try {
-                            
-                            if (image_id != null) {
-                                System.out.println("image id not null "+ image_id );
-                                int img_id = Integer.parseInt(image_id);
-                                System.out.println("image converted to integer");
-                                PreparedStatement statement2 = Connection1.prepareStatement("SELECT image_id,prt_img from PRT_GEN_IMAGE where image_id=?");
-                                statement2.setInt(1,img_id);
-
-                                //statement.setInt(1, new Integer(imageId));
-                                ResultSet rs2 = statement2.executeQuery();
-                                if (rs2.next()) {
-                                    System.out.println("record image found");
-                                    Blob blob = rs2.getBlob("prt_img");
-                                    BufferedInputStream in = new BufferedInputStream(blob.getBinaryStream());
-                                    int b;
-                                    byte[] buffer = new byte[10240];
-                                    while ((b = in.read(buffer, 0, 10240)) != -1) {
-                                        os.write(buffer, 0, b);
-                                    }
-                                    os.close();
-                                }
-                                
-                            }
-                        }
-                        catch(Exception e) {
-                            System.out.println("internal query error");
-                        }
-                        
-                        
-                        Connection1.close();
-                        //os.close();
-                    }
-                    else
-                        System.out.println("No record found in image map table");
+                  
                     
                 }
             }
@@ -133,14 +84,6 @@ public class ImageMapServlet extends HttpServlet{
         
 
     }
-
-
-    @Override
-    protected void doPost(HttpServletRequest httpServletRequest,
-                          HttpServletResponse httpServletResponse) throws ServletException,
-                                                                          IOException {
-        //super.doPost(httpServletRequest, httpServletResponse);
-        doGet(httpServletRequest, httpServletResponse);
-        
-    }
+    
+   
 }
