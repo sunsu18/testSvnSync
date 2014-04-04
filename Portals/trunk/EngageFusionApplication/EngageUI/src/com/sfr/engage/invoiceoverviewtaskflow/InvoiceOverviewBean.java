992256b0daf723627ccbe68e8a654a81f5535bfc
pdee <pdee@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-04-04 06:39:09 +0000

    PDEE: 04/04/2014 Added invoiceOverview taskflow and pages.
    git-svn-id: https://10.24.239.14:5511/svn/Engage@298 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/invoiceoverviewtaskflow/InvoiceOverviewBean.java
@@ -0,0 +1,145 @@
+package com.sfr.engage.invoiceoverviewtaskflow;
+
+import com.sfr.engage.authenticatedhometaskflow.AuthenticatedHomeBean;
+
+import com.sfr.engage.model.queries.rvo.PrtAccountRVORowImpl;
+import com.sfr.engage.utility.util.ADFUtils;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+
+import oracle.adf.view.rich.component.rich.input.RichInputDate;
+import oracle.adf.view.rich.component.rich.input.RichSelectOneChoice;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+
+import oracle.jbo.ViewObject;
+
+public class InvoiceOverviewBean implements Serializable {
+    @SuppressWarnings("compatibility")
+    private static final long serialVersionUID = 1L;
+    private transient Bindings bindings;
+    private String accountValue;
+    private String invoiceTypeValue;
+    private ArrayList<SelectItem> accountList = null;
+    private ArrayList<SelectItem> invoiceTypeList = null;
+    
+    public InvoiceOverviewBean() {
+        super();
+    }
+    
+    public ArrayList<SelectItem> getAccountList() {
+        if (accountList == null) {
+            ViewObject vo = ADFUtils.getViewObject("PrtAccountRVO1Iterator");
+            vo.setNamedWhereClauseParam("countryCode", "en_US");
+            vo.executeQuery();
+            accountList = new ArrayList<SelectItem>();
+            while (vo.hasNext()) {
+                PrtAccountRVORowImpl currRow = (PrtAccountRVORowImpl)vo.next();
+                if (currRow.getAccountId() != null) {
+                    SelectItem selectItem = new SelectItem();
+                    selectItem.setLabel(currRow.getAccountId());
+                    selectItem.setValue(currRow.getAccountId());
+                    accountList.add(selectItem);
+                }
+            }
+        }
+        return accountList;
+    }
+    
+    public ArrayList<SelectItem> getInvoiceTypeList() {
+        if (invoiceTypeList == null) {
+//            ViewObject vo = ADFUtils.getViewObject("PrtAccountRVO1Iterator");
+//            vo.setNamedWhereClauseParam("countryCode", "en_US");
+//            vo.executeQuery();
+            invoiceTypeList = new ArrayList<SelectItem>();
+//            while (vo.hasNext()) {
+//                PrtAccountRVORowImpl currRow = (PrtAccountRVORowImpl)vo.next();
+//                if (currRow.getAccountId() != null) {
+//                    SelectItem selectItem = new SelectItem();
+//                    selectItem.setLabel(currRow.getAccountId());
+//                    selectItem.setValue(currRow.getAccountId());
+//                    accountList.add(selectItem);
+//                }
+//            }
+        }
+        return accountList;
+    }
+    
+    /**
+     * @return bindings Object
+     */
+    public Bindings getBindings() {
+        if (bindings == null) {
+            bindings = new Bindings();
+        }
+        return bindings;
+    }
+
+    public void setAccountValue(String accountValue) {
+        this.accountValue = accountValue;
+    }
+
+    public String getAccountValue() {
+        return accountValue;
+    }
+
+    public void setInvoiceTypeValue(String invoiceTypeValue) {
+        this.invoiceTypeValue = invoiceTypeValue;
+    }
+
+    public String getInvoiceTypeValue() {
+        return invoiceTypeValue;
+    }
+
+    public void searchResultsListener(ActionEvent actionEvent) {
+        // Add event code here...
+    }
+
+    public void clearSearchListener(ActionEvent actionEvent) {
+        // Add event code here...
+    }
+
+    public class Bindings {
+        private RichSelectOneChoice account;
+        private RichSelectOneChoice invoiceType;
+        private RichInputDate fromDate;
+        private RichInputDate toDate;
+
+        public void setAccount(RichSelectOneChoice account) {
+            this.account = account;
+        }
+
+        public RichSelectOneChoice getAccount() {
+            return account;
+        }
+
+        public void setInvoiceType(RichSelectOneChoice invoiceType) {
+            this.invoiceType = invoiceType;
+        }
+
+        public RichSelectOneChoice getInvoiceType() {
+            return invoiceType;
+        }
+
+        public void setFromDate(RichInputDate fromDate) {
+            this.fromDate = fromDate;
+        }
+
+        public RichInputDate getFromDate() {
+            return fromDate;
+        }
+
+        public void setToDate(RichInputDate toDate) {
+            this.toDate = toDate;
+        }
+
+        public RichInputDate getToDate() {
+            return toDate;
+        }
+    }
+}
