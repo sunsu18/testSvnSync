f929abcbc588bcc861a3a1c79c7749ac1990b030
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-04-10 09:01:18 +0000

    hitk : VO for Account summary added
    git-svn-id: https://10.24.239.14:5511/svn/Engage@339 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/accountsummarytaskflow/AccountSummary.java
+++ Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/accountsummarytaskflow/AccountSummary.java
@@ -3,6 +3,9 @@ package com.sfr.engage.accountsummarytaskflow;
 import java.util.Iterator;
 import java.util.List;
 
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
 import oracle.adf.view.rich.component.rich.data.RichTree;
 
 import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
@@ -11,6 +14,7 @@ import oracle.adf.view.rich.component.rich.output.RichOutputText;
 import oracle.adf.view.rich.context.AdfFacesContext;
 
 import oracle.jbo.Row;
+import oracle.jbo.ViewObject;
 import oracle.jbo.uicli.binding.JUCtrlHierBinding;
 import oracle.jbo.uicli.binding.JUCtrlHierNodeBinding;
 
@@ -29,6 +33,7 @@ public class AccountSummary {
     private RichPanelGroupLayout companyOverviewTextPanel;
     private String overviewText;
     private RichOutputText overviewOutputText;
+    private String id;
 
 
     public AccountSummary() {
@@ -48,10 +53,23 @@ public class AccountSummary {
         while (rksIterator.hasNext()) {
         List key1 = (List)rksIterator.next();
         JUCtrlHierBinding treeBinding = null;
+        JUCtrlHierBinding treeBinding2 = null;
         CollectionModel collectionModel = (CollectionModel)tree1.getValue();
         treeBinding = (JUCtrlHierBinding)collectionModel.getWrappedData();
+        treeBinding2 = (JUCtrlHierBinding)collectionModel.getRowData();
+        
         nodeBinding1 = treeBinding.findNodeByKeyPath(key1);
-        System.out.println("Node Binding " + nodeBinding1);
+        System.out.println("String 1--------->" + treeBinding.getPath());
+            System.out.println("String 2---------->" + treeBinding.getAllRowsInRange().length);
+        //System.out.println("Node Binding1 " + nodeBinding1.getRowKey().toString());
+        for (Object o : nodeBinding1.getAttributeValues()) {
+            System.out.println("Node Binding2 ");
+        System.out.println("Selected values " + o.toString());
+             id = o.toString();
+        }
+        
+        //System.out.println("Node Binding3 " + nodeBinding1.getRowKey().toString());
+            
         rw = nodeBinding1.getRow();
         rowType = rw.getStructureDef().getDefName();
         System.out.println("treeSelectionListener : " + ".............." + rowType);
@@ -61,6 +79,28 @@ public class AccountSummary {
             System.out.println("Account node clicked");
             companyOverview.setVisible(false);
             AdfFacesContext.getCurrentInstance().addPartialTarget(companyOverview);
+            DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+            DCIteratorBinding iter1;
+           
+            if (bindings != null) {
+                iter1 = bindings.findIteratorBinding("PrtAccountVO2Iterator");
+                System.out.println("DC Iterator bindings for Account found in mypagelistner");
+            } else {
+                System.out.println("account bindings is null");
+                iter1 = null;
+            }
+//            
+            if(iter1!=null)
+            {
+            
+            ViewObject vo_acc = iter1.getViewObject();
+            vo_acc.setWhereClause("ACCOUNT_ID =: accid");
+            vo_acc.defineNamedWhereClauseParam("accid",id,null);
+            vo_acc.setNamedWhereClauseParam("countryCode", "no_NO");
+            System.out.println(vo_acc.getQuery());                                                        
+            vo_acc.executeQuery();
+            
+            }
             accountOverview.setVisible(true);
            AdfFacesContext.getCurrentInstance().addPartialTarget(accountOverview);
             cardOverview.setVisible(false);
@@ -73,8 +113,37 @@ public class AccountSummary {
             System.out.println("cardgroup node clicked");
             companyOverview.setVisible(false);
             AdfFacesContext.getCurrentInstance().addPartialTarget(companyOverview);
-            cardGroupOverview.setVisible(true);
+            DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+            DCIteratorBinding iter1;
             
+            if (bindings != null) {
+                iter1 = bindings.findIteratorBinding("PrtCardgroupVO3Iterator");
+                System.out.println("DC Iterator bindings for CardGroup found in mypagelistner");
+            } else {
+                System.out.println("card group bindings is null");
+                iter1 = null;
+            }
+            //
+            if(iter1!=null)
+            {
+            
+            ViewObject vo_cg = iter1.getViewObject();
+            vo_cg.setWhereClause("CARDGROUP_SEQ =: cgid AND COUNTRY_CODE =: cc");
+
+            //vo_cg.setWhereClause("CARDGROUP_SEQ =: cgid");
+            vo_cg.defineNamedWhereClauseParam("cgid",id,null);
+           // vo_cg.setWhereClause("COUNTRY_CODE =: cc");
+            vo_cg.defineNamedWhereClauseParam("cc","no_NO",null);
+         
+            System.out.println(vo_cg.getQuery());                                                        
+            vo_cg.executeQuery();
+            
+            }
+            
+            
+            
+            
+            cardGroupOverview.setVisible(true);
             AdfFacesContext.getCurrentInstance().addPartialTarget(cardGroupOverview);
             accountOverview.setVisible(false);
             AdfFacesContext.getCurrentInstance().addPartialTarget(accountOverview);
@@ -86,8 +155,34 @@ public class AccountSummary {
             System.out.println("card node clicked");
             companyOverview.setVisible(false);
             AdfFacesContext.getCurrentInstance().addPartialTarget(companyOverview);
+            
+            DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+            DCIteratorBinding iter1;
+            
+            if (bindings != null) {
+                iter1 = bindings.findIteratorBinding("PrtCardVO4Iterator");
+                System.out.println("DC Iterator bindings for Card found in mypagelistner");
+            } else {
+                System.out.println("card bindings is null");
+                iter1 = null;
+            }
+            //
+            if(iter1!=null)
+            {
+            
+            ViewObject vo_card = iter1.getViewObject();
+            vo_card.setWhereClause("PRT_CARD_PK =: cardid AND COUNTRY_CODE =: cc");
+            //vo_card.setWhereClause("PRT_CARD_PK =: cardid");
+            vo_card.defineNamedWhereClauseParam("cardid",id,null);
+           //s vo_card.setWhereClause("COUNTRY_CODE =: cc");
+            vo_card.defineNamedWhereClauseParam("cc","no_NO",null);
+            
+            System.out.println(vo_card.getQuery());                                                        
+            vo_card.executeQuery();
+            
+            }
+            
             cardOverview.setVisible(true);
-           
             AdfFacesContext.getCurrentInstance().addPartialTarget(cardOverview);
             accountOverview.setVisible(false);
             AdfFacesContext.getCurrentInstance().addPartialTarget(accountOverview);
@@ -98,6 +193,34 @@ public class AccountSummary {
         }
         else {
             System.out.println("partner node clicked");
+            
+//            DCBindingContainer bindings = (DCBindingContainer)BindingContext.getCurrent().getCurrentBindingsEntry();
+//            DCIteratorBinding iter1;
+//            
+//            if (bindings != null) {
+//                iter1 = bindings.findIteratorBinding("PrtPartnerVO1Iterator");
+//                System.out.println("DC Iterator bindings for Card found in mypagelistner");
+//            } else {
+//                System.out.println("card bindings is null");
+//                iter1 = null;
+//            }
+//            //
+//            if(iter1!=null)
+//            {
+//            
+//            ViewObject vo_card = iter1.getViewObject();
+//            vo_part.setWhereClause("PARTNER_ID =: partid");
+//            vo_part.defineNamedWhereClauseParam("partid",id,null);
+//            vo_part.setWhereClause("COUNTRY_CODE =: cc");
+//            vo_part.defineNamedWhereClauseParam("cc","no_NO",null);
+//            
+//            System.out.println(vo_part.getQuery());                                                        
+//            vo_part.executeQuery();
+//            
+//            }
+            
+            
+            
             companyOverview.setVisible(true);
             AdfFacesContext.getCurrentInstance().addPartialTarget(companyOverview);
             cardOverview.setVisible(false);
@@ -191,5 +314,13 @@ public class AccountSummary {
 
     public RichPanelGroupLayout getCompanyOverviewTextPanel() {
         return companyOverviewTextPanel;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
     }
 }
