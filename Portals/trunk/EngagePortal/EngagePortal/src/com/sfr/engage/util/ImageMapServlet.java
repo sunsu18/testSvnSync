978e1f2cdda3c302c1868b9768ae6ede3622f479
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-05-09 15:52:04 +0000

    hitk : handled default image concept in image servlet
    git-svn-id: https://10.24.239.14:5511/svn/Engage@577 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngagePortal/EngagePortal/src/com/sfr/engage/util/ImageMapServlet.java
+++ Portals/trunk/EngagePortal/EngagePortal/src/com/sfr/engage/util/ImageMapServlet.java
@@ -3,6 +3,8 @@ package com.sfr.engage.util;
 
 import com.sfr.engage.services.core.dao.factory.DAOFactory;
 
+import com.sfr.util.AccessDataControl;
+
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,11 +14,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import java.sql.SQLException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import oracle.adf.share.logging.ADFLogger;
 
 /**
  * TODO : ASHTHA - 02, May, 2014 :
@@ -28,6 +34,10 @@ public class ImageMapServlet extends HttpServlet {
     private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
     @SuppressWarnings("compatibility")
     private static final long serialVersionUID = 1L;
+    
+    AccessDataControl accessDC = new AccessDataControl();
+    
+    public static final ADFLogger log = AccessDataControl.getSFRLogger();
 
     /**
      */
@@ -49,18 +59,20 @@ public class ImageMapServlet extends HttpServlet {
      * @throws ServletException
      * @throws IOException
      */
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        
+        log.fine(accessDC.getDisplayRecord()+ this.getClass() + "Inside doGET method of ImageServlet");
         OutputStream os = response.getOutputStream();
         Connection connection1 = null; 
         response.setContentType(CONTENT_TYPE);
 
-        // TODO : ASHTHA - 02, May, 2014 : SOP format should always be followed
-        System.out.println("Request1 -----------------------> " + request.getParameter("param1"));
-        System.out.println("Request2 -----------------------> " + request.getParameter("param2"));
-        System.out.println("Request3 -----------------------> " + request.getParameter("param3"));
-        System.out.println("Request4 -----------------------> " + request.getParameter("param4"));
-        System.out.println("Request5 -----------------------> " + request.getParameter("param5"));
-
+        
+//        System.out.println("Request1 -----------------------> " + request.getParameter("param1"));
+//        System.out.println("Request2 -----------------------> " + request.getParameter("param2"));
+//        System.out.println("Request3 -----------------------> " + request.getParameter("param3"));
+//        System.out.println("Request4 -----------------------> " + request.getParameter("param4"));
+//        System.out.println("Request5 -----------------------> " + request.getParameter("param5"));
+        log.fine(accessDC.getDisplayRecord()+ this.getClass() + "Param1 = " + request.getParameter("param1") + "Param2 = " + request.getParameter("param2") + "Param3 = " + request.getParameter("param3") + "Param4 = " + request.getParameter("param4") + "Param5 = " + request.getParameter("param5"));
         String featureName = request.getParameter("param1"); 
         String featureValue = request.getParameter("param2"); 
         String controlAttr = request.getParameter("param3"); 
@@ -68,7 +80,7 @@ public class ImageMapServlet extends HttpServlet {
         String controlAttrValue; 
         if (featureName.equalsIgnoreCase("LISTPRICE")) { // TODO : ASHTHA - 02, May, 2014 : Avoid hardcoding
             controlAttrValue = "41S5" + attrValue;
-            System.out.println("Control_Attr_Value" + controlAttrValue); 
+            //System.out.println("controlAttrValue" + controlAttrValue); 
         } else {
             controlAttrValue = null;
         }
@@ -90,21 +102,24 @@ public class ImageMapServlet extends HttpServlet {
                 //statement.setInt(1, new Integer(imageId));
                 ResultSet rs = statement.executeQuery();
                 if (rs.next()) {
-                    System.out.println("record found in image map table with image id " +rs.getString("imageid")); // TODO : ASHTHA - 02, May, 2014 : Use logger
+                    
+                    
                     String image_id = rs.getString("imageid");
 
                     try {
 
                         if (image_id != null) {
-                            System.out.println("image id not null " + image_id); // TODO : ASHTHA - 02, May, 2014 : Use logger
+                            log.info(accessDC.getDisplayRecord()+ this.getClass() + "Record found in image map table with image id " + image_id);
+                            
                             int img_id = Integer.parseInt(image_id);
-                            System.out.println("image converted to integer"); // TODO : ASHTHA - 02, May, 2014 : Use logger
+                            
                             PreparedStatement statement2 = connection1.prepareStatement("SELECT image_id,prt_img from PRT_GEN_IMAGE where image_id=?");
                             statement2.setInt(1, img_id);
 
                             //statement.setInt(1, new Integer(imageId));
                             ResultSet rs2 = statement2.executeQuery();
                             if (rs2.next()) {
+                                log.info(accessDC.getDisplayRecord()+ this.getClass() + "Record image found for " + img_id);
                                 System.out.println("record image found"); // TODO : ASHTHA - 02, May, 2014 : Use logger
                                 Blob blob = rs2.getBlob("prt_img");
                                 BufferedInputStream in = new BufferedInputStream(blob.getBinaryStream());
@@ -113,17 +128,47 @@ public class ImageMapServlet extends HttpServlet {
                                 while ((b = in.read(buffer, 0, 10240)) != -1) {
                                     os.write(buffer, 0, b);
                                 }
-                                os.close(); // TODO : ASHTHA - 02, May, 2014 : Move to Finally block
-                                connection1.close(); // TODO : ASHTHA - 02, May, 2014 : Connection SHOULD ALWAYS be closed in Finally block
+                                
                             }
 
-                        } // TODO : ASHTHA - 02, May, 2014 : if imageId is null, shouldn't this case be handled witha default imageId?
+                        }
+                        else
+                        {
+                            image_id = "102";
+                            //if imageId is null, then to fetch default imageId?
+                            int img_id = Integer.parseInt(image_id);
+                            
+                            PreparedStatement statement2 = connection1.prepareStatement("SELECT image_id,prt_img from PRT_GEN_IMAGE where image_id=?");
+                            statement2.setInt(1, img_id);
+
+                            //statement.setInt(1, new Integer(imageId));
+                            ResultSet rs2 = statement2.executeQuery();
+                            if (rs2.next()) {
+                                log.info(accessDC.getDisplayRecord()+ this.getClass() + "Record image found for " + img_id);
+                                System.out.println("record image found"); // TODO : ASHTHA - 02, May, 2014 : Use logger
+                                Blob blob = rs2.getBlob("prt_img");
+                                BufferedInputStream in = new BufferedInputStream(blob.getBinaryStream());
+                                int b;
+                                byte[] buffer = new byte[10240];
+                                while ((b = in.read(buffer, 0, 10240)) != -1) {
+                                    os.write(buffer, 0, b);
+                                }
+                                
+                            }
+                            
+                        }
                     } catch (Exception e) {
-                        System.out.println("internal query error"); // TODO : ASHTHA - 02, May, 2014 : Use logger
+                        log.severe(accessDC.getDisplayRecord()+ this.getClass() + "Internal Query Error for PRT_GEN_IMAGE table " + e.getMessage());
+                        
+                    }
+                    finally {
+                        os.close(); 
+                        connection1.close(); 
+                        
                     }
 
-                    connection1.close(); // TODO : ASHTHA - 02, May, 2014 : Connection SHOULD ALWAYS be closed in Finally block
-                    //os.close();
+                    
+                    
                 } else
                 {
                     System.out.println("No record found in image map table"); // TODO : ASHTHA - 02, May, 2014 : Use logger
@@ -132,7 +177,13 @@ public class ImageMapServlet extends HttpServlet {
 
             }
         } catch (Exception e) {
-            System.out.println("Exception " + e); // TODO : ASHTHA - 02, May, 2014 : Use logger
+            log.severe(accessDC.getDisplayRecord()+ this.getClass() + "External Query Error for PRT_GEN_IMAGE_MAP table " + e.getMessage());
+            
         }
+       
+        
+        log.fine(accessDC.getDisplayRecord()+ this.getClass() + "Exiting from doGET method of ImageServlet");
     }
+    
+    
 }
