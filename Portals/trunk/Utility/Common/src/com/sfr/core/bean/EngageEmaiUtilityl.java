b905d05a5773d66ce8410e4c288693a1e404ec4b
ashtha <ashtha@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-06-09 14:25:48 +0000

    hitk " new mail utility
    git-svn-id: https://10.24.239.14:5511/svn/Engage@942 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/trunk/Utility/Common/src/com/sfr/core/bean/EngageEmaiUtilityl.java
@@ -0,0 +1,114 @@
+package com.sfr.core.bean;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+public class EngageEmaiUtilityl {
+    public EngageEmaiUtilityl() {
+        super();
+        System.out.println("INside constructor");
+    }
+    public void sendEmail(String from,String to,String subject,String body,String protocol,String smtphost,String cc,byte[] responseByteArr,String envprop,String filename) throws MessagingException
+    {
+    // Add event code here...
+    System.out.println("sending email to " + to + "for invoice " + filename +"having byte array size as"+ responseByteArr.length);
+    System.out.println(from + " "+ subject + " " + protocol + " " + smtphost + " " + cc + " "+envprop);
+    Properties props = new Properties();
+    props.setProperty("mail.transport.protocol", protocol);
+    props.setProperty("mail.host", smtphost);
+
+    Session mailSession = Session.getDefaultInstance(props, null);
+        try {
+
+                // Create a default MimeMessage object.
+                MimeMessage message = new MimeMessage(mailSession);
+
+                // Set From: header field of the header.
+                message.setFrom(new InternetAddress(from));
+
+                //CC
+                //message.setRecipients(Message.RecipientType.CC, cc);
+
+                // Set To: header field of the header.
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+                // Set Subject: header field
+                message.setSubject(subject);
+
+                // Create the message part
+                BodyPart messageBodyPart = new MimeBodyPart();
+
+                // Fill the message
+    //                File file = new File(envProperty);
+    //            System.out.println("File path : " + envProperty);
+    //                System.out.println("EmailUtility.sendEmail : "+"Checking if file at " + envProperty + " does really exists =" + file.exists());
+    //                DataSource datasource = new FileDataSource(envProperty);
+    //                messageBodyPart.setDataHandler(new DataHandler(datasource));
+    //                messageBodyPart.setHeader("Content-ID", "statoil");
+                messageBodyPart.setContent(body, "text/html");
+
+                // Create a multipar message
+                Multipart multipart = new MimeMultipart();
+
+                // Set text message part
+                multipart.addBodyPart(messageBodyPart);
+
+                // Part two is attachment
+                messageBodyPart = new MimeBodyPart();
+                //String filename = "C:\\JDeveloper\\mywork\\SendEmail\\Project1\\public_html\\image\\addc.PNG";
+                //String filename2="C:\\Users\\10604350\\Desktop\\Test_Case_WS.docx";
+                //DataSource source = new FileDataSource(filename2);
+                System.out.println("Length 2"+ responseByteArr.length);
+                DataSource source = new ByteArrayDataSource(responseByteArr,"application/pdf");
+                messageBodyPart.setDataHandler(new DataHandler(source));
+                //String filename = filename.concat(".pdf");
+                messageBodyPart.setFileName(filename.concat(".pdf"));
+
+                multipart.addBodyPart(messageBodyPart);
+
+
+                // Send the complete message parts
+                message.setContent(multipart,"text/html");
+
+
+                messageBodyPart = new MimeBodyPart();
+                DataSource fds = new FileDataSource(envprop);
+
+                messageBodyPart.setDataHandler(new DataHandler(fds));
+                messageBodyPart.setHeader("Content-ID", "image");
+
+                    multipart.addBodyPart(messageBodyPart);
+
+
+                    // Send the complete message parts
+                    message.setContent(multipart,"text/html");
+
+
+    //message.setContent(body,"text/html");
+
+
+                // Send message
+                Transport.send(message);
+                System.out.println("Sent message successfully....");
+            }
+        catch(MessagingException mex){
+            mex.printStackTrace();
+        }
+
+    }
+}
