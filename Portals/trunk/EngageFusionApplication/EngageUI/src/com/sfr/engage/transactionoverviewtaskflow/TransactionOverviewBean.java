8e66a8dbe28d87e055d637151baf05a8f781b8a2
pdee <pdee@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-06-17 06:24:26 +0000

    PDEE: 12/06/2014 Added loggers.
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1033 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/transactionoverviewtaskflow/TransactionOverviewBean.java
+++ Portals/trunk/EngageFusionApplication/EngageUI/src/com/sfr/engage/transactionoverviewtaskflow/TransactionOverviewBean.java
@@ -268,18 +268,28 @@ public class TransactionOverviewBean implements Serializable {
         if (lang == "NO") {
             currencyCode = conversionUtility.getCurrencyCode(lang);
             locale = conversionUtility.getLocaleFromCountryCode(lang);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"currencyCode :" + currencyCode);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"Locale :" + locale);
         } else if (lang == "SE") {
             currencyCode = conversionUtility.getCurrencyCode(lang);
             locale = conversionUtility.getLocaleFromCountryCode(lang);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"currencyCode :" + currencyCode);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"Locale :" + locale);
         }else if (lang == "DK") {
             currencyCode = conversionUtility.getCurrencyCode(lang);
             locale = conversionUtility.getLocaleFromCountryCode(lang);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"currencyCode :" + currencyCode);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"Locale :" + locale);
         }else if (lang == "PL") {
             currencyCode = conversionUtility.getCurrencyCode(lang);
             locale = conversionUtility.getLocaleFromCountryCode(lang);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"currencyCode :" + currencyCode);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"Locale :" + locale);
         }else {
             currencyCode = conversionUtility.getCurrencyCode("SE");
             locale = conversionUtility.getLocaleFromCountryCode("SE");
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"Default:currencyCode :" + currencyCode);
+            _logger.info(accessDC.getDisplayRecord() + this.getClass() + " " +"Default:Locale :" + locale);
         }
         
        
