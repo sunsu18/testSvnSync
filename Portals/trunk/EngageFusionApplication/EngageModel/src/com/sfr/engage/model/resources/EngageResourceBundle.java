acaa931d4fcd8dfcaad4497d639bd126c9220b0b
komt <komt@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-07-03 11:20:25 +0000

    KOMT: 03/07/2014- ADDED note for view cards page
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1243 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageFusionApplication/EngageModel/src/com/sfr/engage/model/resources/EngageResourceBundle.java
+++ Portals/trunk/EngageFusionApplication/EngageModel/src/com/sfr/engage/model/resources/EngageResourceBundle.java
@@ -202,7 +202,7 @@ public class EngageResourceBundle extends ListResourceBundle{
                                       {"MAIL_SUCCESSFUL",""},{"MAIL_NOT_FOUND",""},{"INVOICE_NOT_FOUND",""},
                                       {"CARD_TEXTLINE_2",""},{"VEHICLE_DISASSOCIATED",""},{"DRIVER_DISASSOCIATED",""},{"RESET",""},{"COUNTRY_HUNGARY",""},
                                       {"COUNTRY_ROMANIA",""},{"COUNTRY_BULGARIA",""},{"COUNTRY_GERMANY",""},{"COUNTRY_BOSNIA",""},{"COUNTRY_POLAND",""},
-                                      {"PALS_ODOMETER",""},{"CARD_TYPE_DESCRIPTION",""},{"ENG_LAST_USED",""},{"CARDGROUP_DESCRIPTION",""},{"CARDGROUP_ID",""}};
+                                      {"PALS_ODOMETER",""},{"CARD_TYPE_DESCRIPTION",""},{"ENG_LAST_USED",""},{"CARDGROUP_DESCRIPTION",""},{"CARDGROUP_ID",""},{"ENG_VIEW_CARD_NOTE",""}};
 
     /**
      * This method populates the values of all resource keys from database through PrtGenTranslationVO based on lang parameter.
