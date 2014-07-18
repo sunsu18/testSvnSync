8899d2ae36828f027c5c6e56245449def38cad6b
pdee <pdee@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-07-18 08:33:00 +0000

    PDEE: 07/18/2014 Added Report Localization functionality..
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1340 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/ReportBundle.java
+++ Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/ReportBundle.java
@@ -13,6 +13,10 @@ import oracle.jdbc.OracleTypes;
 
 import java.util.HashMap;
 
+import java.util.Iterator;
+
+import java.util.Map;
+
 import javax.naming.InitialContext;
 
 import javax.sql.DataSource;
@@ -28,7 +32,7 @@ public class ReportBundle {
         String value = "";
         String reportColumns ="";
         ResultSet reportRS;
-        HashMap<String, String> map = parseHashMap(colList);
+        HashMap<String, String> mapDB = new HashMap<String, String>();        
             try {
             InitialContext ctx = new InitialContext();
             DataSource ds = (DataSource)ctx.lookup("jdbc/engagecustom");
@@ -43,42 +47,47 @@ public class ReportBundle {
                  cst.executeQuery();
     
             // get cursor and cast it to ResultSet
-             reportRS = (ResultSet) cst.getObject(2);
-            while(reportRS.next())
-            {
-               if("NO".equalsIgnoreCase(countryCode))
-               {
-                   key=reportRS.getString("NO_NO");
-                   value = reportRS.getString("EN");                  
-               }else{
-                   key=reportRS.getString(countryCode);
-                   value = reportRS.getString("EN");
-               }                
-                if (map.containsKey(key)) {
-                    reportColumns=reportColumns+value+"|";
+             reportRS = (ResultSet) cst.getObject(2);                
+            
+           
+                while (reportRS.next()) {
+                    if ("NO".equalsIgnoreCase(countryCode)) {
+                        key = reportRS.getString("NO_NO");
+                        value = reportRS.getString("EN");
+                    } else {
+                        key = reportRS.getString(countryCode);
+                        value = reportRS.getString("EN");
+                    }
+                    System.out.println("Value ="+value);
+                    mapDB.put(key, value); 
                 }
-                                
-            }            
+
+            String[] headerValues =
+                colList.split(Constants.ENGAGE_REPORT_DELIMITER);
+            for (int col = 0; col < headerValues.length; col++) {
+                System.out.println("String Value Form=" +
+                                   headerValues[col].toString());
+
+
+                for (Map.Entry<String, String> entryDB : mapDB.entrySet()) {
+                    String keyDB = entryDB.getKey();
+                    String valueDB = entryDB.getValue();
+                    if (headerValues[col].toString().trim().equalsIgnoreCase(keyDB.trim())) {
+                        reportColumns = reportColumns + valueDB + "|";
+                    }
+                }
+            }
+           if(reportColumns != null && reportColumns.equals(""))
+           {
             reportColumns=reportColumns.substring(0, reportColumns.length()-1);
+            
+           }
                 
             conn.close();
         } catch (Exception e) {            
             e.printStackTrace();
         }   
         return reportColumns;
-    }
+    }  
     
-    
-    private HashMap<String, String> parseHashMap(String params) throws Exception {
-        try {
-            String[] headerValues = params.split(Constants.ENGAGE_REPORT_DELIMITER);
-            HashMap<String, String> map = new HashMap<String, String>();
-            for (int col = 0; col < headerValues.length; col++) {  
-                map.put(headerValues[col].toString(), "");
-            }            
-            return map;
-        } catch (Exception e) {           
-            throw e;
-        }
-    }    
 }
