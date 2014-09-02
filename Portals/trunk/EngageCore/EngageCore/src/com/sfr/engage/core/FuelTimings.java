e57bfc31336900b289f460bd0f7118a695d93f53
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-09-02 05:57:05 +0000

    HITK : ALERTS USER STORY RELATED CORE CLASSES
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1569 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/FuelTimings.java
+++ Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/FuelTimings.java
@@ -5,8 +5,10 @@ public class FuelTimings {
         super();
     }
     private String weekday;
-    private String from;
-    private String to;
+    private String fromHh;
+    private String fromMm;
+    private String toHh;
+    private String toMm;
 
     public void setWeekday(String weekday) {
         this.weekday = weekday;
@@ -16,19 +18,36 @@ public class FuelTimings {
         return weekday;
     }
 
-    public void setFrom(String from) {
-        this.from = from;
+
+    public void setFromHh(String fromHh) {
+        this.fromHh = fromHh;
     }
 
-    public String getFrom() {
-        return from;
+    public String getFromHh() {
+        return fromHh;
     }
 
-    public void setTo(String to) {
-        this.to = to;
+    public void setFromMm(String fromMm) {
+        this.fromMm = fromMm;
     }
 
-    public String getTo() {
-        return to;
+    public String getFromMm() {
+        return fromMm;
+    }
+
+    public void setToHh(String toHh) {
+        this.toHh = toHh;
+    }
+
+    public String getToHh() {
+        return toHh;
+    }
+
+    public void setToMm(String toMm) {
+        this.toMm = toMm;
+    }
+
+    public String getToMm() {
+        return toMm;
     }
 }
