32974c5c857ce9253fed935258b7084c50addd97
komt <komt@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-09-19 15:23:14 +0000

    TECH: changes reverted for night deployment issues
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1651 f308e16d-57e9-4fa7-a310-9aba3baf408f

--- Portals/trunk/Build/deploy-engageportal-to-Dev.py
+++ Portals/trunk/Build/deploy-engageportal-to-Dev.py
@@ -21,9 +21,5 @@ artifactLocation=r'C:\Users\10607024\.jenkins\jobs\Engage Build and Deployment\w
 print ("Hello, Python!")
 print "Hello, Python!"
 connect(adminUser,adminPassword,adminUrl)
-startEdit()
 deploy(applicationName,artifactLocation,targets=targetsName,stageMode=stageModeValue,upload='true',remote='true')
-save()
-activate()
-stopEdit()
 disconnect()
