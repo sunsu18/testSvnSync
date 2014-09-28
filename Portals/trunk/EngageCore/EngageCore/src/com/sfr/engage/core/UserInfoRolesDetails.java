f586669f2caf763a9c30c9a8206090610f30ef8f
sidg <sidg@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-09-28 10:26:53 +0000

    US 494 & 558: Added a new core class UserInfoRolesDetails.java and used in logic implementation in User info story
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1695 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/UserInfoRolesDetails.java
@@ -0,0 +1,49 @@
+package com.sfr.engage.core;
+
+import java.io.Serializable;
+
+public class UserInfoRolesDetails implements Serializable {
+    @SuppressWarnings("compatibility:8035622705877005723")
+    private static final long serialVersionUID = 1L;
+
+    private String roleName;
+    private String associationValue;
+    private String partnerId;
+    private boolean checkUserRole;
+
+    public UserInfoRolesDetails() {
+        super();
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setAssociationValue(String associationValue) {
+        this.associationValue = associationValue;
+    }
+
+    public String getAssociationValue() {
+        return associationValue;
+    }
+
+    public void setPartnerId(String partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    public String getPartnerId() {
+        return partnerId;
+    }
+
+    public void setCheckUserRole(boolean checkUserRole) {
+        this.checkUserRole = checkUserRole;
+    }
+
+    public boolean isCheckUserRole() {
+        return checkUserRole;
+    }
+}
