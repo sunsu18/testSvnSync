28ff0b8fb100d41942656e0d390c5df0cfce871b
hitk <hitk@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-09-29 05:55:37 +0000

    US527 : Delete alert (core classes for proxy req & response)
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1703 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/trunk/EngageCore/EngageCore/src/com/sfr/engage/core/AlertsUnsubscribeResponse.java
@@ -0,0 +1,16 @@
+package com.sfr.engage.core;
+
+public class AlertsUnsubscribeResponse {
+    public AlertsUnsubscribeResponse() {
+        super();
+    }
+    private String status;
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
