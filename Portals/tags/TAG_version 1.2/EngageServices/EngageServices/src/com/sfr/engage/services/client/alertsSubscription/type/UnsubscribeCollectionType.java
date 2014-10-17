443f6724090a2121b87587428a5a1b489612aa85
sunsu <sunsu@f308e16d-57e9-4fa7-a310-9aba3baf408f>
2014-10-17 10:54:05 +0000

    Tagging successful
    git-svn-id: https://10.24.239.14:5511/svn/Engage@1799 f308e16d-57e9-4fa7-a310-9aba3baf408f

---
+++ Portals/tags/TAG_version 1.2/EngageServices/EngageServices/src/com/sfr/engage/services/client/alertsSubscription/type/UnsubscribeCollectionType.java
@@ -0,0 +1,94 @@
+
+package com.sfr.engage.services.client.alertsSubscription.type;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for UnsubscribeCollectionType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="UnsubscribeCollectionType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="SubscriptionID" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="CustomerID" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "UnsubscribeCollectionType", propOrder = {
+    "subscriptionID",
+    "customerID"
+})
+@XmlSeeAlso({
+    UnsubscribeResponseCollectionType.class
+})
+public class UnsubscribeCollectionType {
+
+    @XmlElement(name = "SubscriptionID", required = true)
+    protected String subscriptionID;
+    @XmlElement(name = "CustomerID", required = true)
+    protected String customerID;
+
+    /**
+     * Gets the value of the subscriptionID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSubscriptionID() {
+        return subscriptionID;
+    }
+
+    /**
+     * Sets the value of the subscriptionID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSubscriptionID(String value) {
+        this.subscriptionID = value;
+    }
+
+    /**
+     * Gets the value of the customerID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    /**
+     * Sets the value of the customerID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCustomerID(String value) {
+        this.customerID = value;
+    }
+
+}
